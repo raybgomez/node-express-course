97ab0440ba82b25e9209098bae6d61c78d7febc0
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-07-26 19:33:38 -0400

    complete week 4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,22 +3,40 @@
 const express = require('express');
 const { products } = require('./data');
 const app = express();
+const people = require('./data.js')
+const peopleRouter = require('./routes/people');
 
-app.use(express.static("./public"));
 
-app.get('/api/v1/test', (req, res) => {
+const logger = (req, res, next) => {
+    const currentTime = new Date().toISOString();
+    console.log(`[${currentTime}] ${req.method} ${req.url}`);
+    next();
+};
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(logger);
+
+app.use(express.static("./methods-public"));
+
+app.get('/', logger, (req, res) => {
+    res.send('Welcome to the home page');
+});
+
+app.get('/api/v1/test', logger, (req, res) => {
     res.json({ message: 'It worked!' });
 });
 
-app.get('/api/v1/test', (req, res) => {
+app.get('/api/v1/test', logger, (req, res) => {
     res.json(products);
 });
 
-app.get('/api/v1/test', (req, res) => {
+app.get('/api/v1/test', logger, (req, res) => {
     res.json(req.params);
 });
 
-app.get('/api/v1/products/:productID', (req, res) => {
+app.get('/api/v1/products/:productID', logger, (req, res) => {
     const idToFind = parseInt(req.params.productID);
     const product = products.find((p) => p.id === idToFind);
 
@@ -29,7 +47,7 @@ app.get('/api/v1/products/:productID', (req, res) => {
     }
 });
 
-app.get('/api/v1/query', (req, res) => {
+app.get('/api/v1/query', logger, (req, res) => {
     const { search, limit } = req.query;
     let filteredProducts = products;
 
@@ -46,7 +64,24 @@ app.get('/api/v1/query', (req, res) => {
     res.json(filteredProducts);
 });
 
-app.all('*', (req, res) => {
+app.use("/api/v1/people", peopleRouter);
+
+
+app.get('/api/v1/people', (req, res) => {
+    res.json(people);
+});
+
+app.post('/api/v1/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, message: "Please provide a name" });
+    }
+    const newPerson = { id: people.length + 1, name };
+    people.push(newPerson);
+    res.status(201).json({ success: true, name });
+});
+
+app.all('*', logger, (req, res) => {
     res.status(404).send('Not Found');
 });
 
